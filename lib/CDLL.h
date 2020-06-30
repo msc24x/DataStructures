1fe8f0c69cfe6ec0795328a15c77a3f4e28a7059
msc24x <51400654+msc24x@users.noreply.github.com>
2020-06-30 18:52:03 +0530

    COMPLETE CDLL
    main.cpp contains the command-line implementation of CDLL.

--- lib/CDLL.h
+++ lib/CDLL.h
@@ -1,9 +1,12 @@
+//  Circular Doubly Linked List
+//----------SOURCE-CODE----------
+//  Author : Maninderpal Singh
+//  https://github.com/msc24x
+
 #ifndef CDLL_H
 #define CDLL_H
 #include <iostream>
-#include <vector>
 
-using namespace std;
 
 template <typename T> class node
 {
@@ -17,23 +20,29 @@ template <typename T> class CDLL
 {
     public:
         int perimeter = 0;
+        node<T>* head = NULL;
 
         void push(T value);
         void pop();
         T first();
         T next();
+        T prev();
         void seek(node<T>* node);
         void seek(int times);
         bool contains(T value);
         int pos(T value);
         int pos(node<T>* node);
         T curr();
+        void insert(T value, int index);
+        void destroyPos(int index);
+        void destroy(T value);
+        void wash();
 
 
 
     private:
-        node<T>* head = NULL;
-        node<T>* curr_node = head;
+
+        node<T>* curr_node = new node<T>;
         node<T>* temp = new node<T>;
 
         bool EmptyCheck(node<T>* node);
@@ -43,12 +52,107 @@ template <typename T> class CDLL
 };
 
 template<typename T>
+void CDLL<T>::wash()
+{
+    while(perimeter) pop();
+}
+
+template<typename T>
+void CDLL<T>::destroy(T value)
+{
+    if(head != NULL)
+    {
+        temp = head;
+        do{
+            if(temp->val == value)
+            {
+                if(temp == head)
+                {
+                    destroyPos(0);
+                }else{
+                    node<T>* d = new node<T>;
+                    d = temp;
+                    link(temp->prev, temp->next);perimeter--;
+                    delete d;
+                }
+            }
+            temp = temp->next;
+            break;
+        }while(temp != head);
+    }
+}
+
+template<typename T>
+void CDLL<T>::destroyPos(int index)
+{
+    if(index > perimeter)
+        index %= perimeter;
+    if(perimeter == 1 && index == 0)
+        pop();
+    else if(head != NULL)
+    {
+        if(index == 0)
+        {
+            node<T>* d = new node<T>;
+            temp = head->next;
+            d = head;
+            link(head->prev, head->next);perimeter--;
+            delete d;
+            head = temp;
+
+        }else {
+            seek(head);
+            seek(index);
+            node<T>* d = new node<T>;
+            d = curr_node;
+            link(curr_node->prev, curr_node->next); perimeter--;
+            delete d;
+        }
+    }
+}
+
+template<typename T>
+void CDLL<T>::insert(T value, int index)
+{
+    if(index > perimeter && perimeter != 0)
+        index %= perimeter;
+    if(head != NULL)
+    {
+        node<T>* newnode = new node<T>;
+        newnode->val = value;
+        seek(head);
+        seek(index);
+        link(curr_node->prev, newnode);
+        link(newnode, curr_node);
+        perimeter++;
+    }
+    else{
+        push(value);
+    }
+}
+
+
+template<typename T>
+T CDLL<T>::prev()
+{
+    if(head != NULL){
+        if(curr_node->prev != NULL)
+        {
+            curr_node = curr_node->prev;
+            return curr_node->val;
+        }
+    }
+    return NULL;
+}
+
+template<typename T>
 T CDLL<T>::curr()
 {
     if(curr_node != NULL)
     {
         return curr_node->val;
     }
+    return NULL;
 }
 
 template<typename T>
@@ -137,8 +241,8 @@ bool CDLL<T>::EmptyCheck(node<T>* node)
 template<typename T >
 T CDLL<T>::next()
 {
-    if(EmptyCheck(head)){
-        if(EmptyCheck(curr_node->next))
+    if(head != NULL){
+        if(curr_node->next != NULL)
         {
             curr_node = curr_node->next;
             return curr_node->val;
@@ -150,7 +254,7 @@ T CDLL<T>::next()
 template<typename T >
 T CDLL<T>::first()
 {
-    if(EmptyCheck(head)) return head->val;
+    if(head != NULL) return head->val;
     return NULL;
 }
 
@@ -158,8 +262,8 @@ T CDLL<T>::first()
 template<typename T >
 void CDLL<T>::pop()
 {
-    if(!EmptyCheck(head)){}
-    else if(head->prev == head)
+    if(head == NULL){}
+    else if(head->prev == head || head->next == head)
     {
         head = NULL;
         perimeter = 0;
@@ -181,7 +285,10 @@ void CDLL<T>::push(T value)
     newnode->val = value;
     newnode->next = newnode;
     if(head == NULL)
+    {
         head = newnode;
+        curr_node = head;
+    }
     else
     {
         link(head->prev, newnode);
--- lib/main.cpp
+++ lib/main.cpp
@@ -1,38 +1,39 @@
+//  Circular Doubly Linked List
+// ----COMMANDLINE-INTERFACE----
+//  Author : Maninderpal Singh
+//  https://github.com/msc24x
+
 #include <iostream>
-#include "structures.h"
+#include "include/CDLL.h"
 using namespace std;
+
+string cmds[] = {"default", "push", "pop", "first", "next", "seek", "contains", "pos", "curr",
+                "perimeter", "cls", "exit", "prev", "insert", "destroy", "destroypos", "show",
+                "help", "wash"};
 
 template <typename T>
 void printit(CDLL<T> *L)
 {
-	cout << '\n';
-	for (int t = 0; t < L->perimeter; t++)
+	cout << "{ ";
+	L->seek(L->head);
+	if(L->perimeter != 0)cout << L->curr() << ", ";
+	else cout << "EMPTY, ";
+	for (int t = 0; t < L->perimeter - 1; t++)
 	{
-		cout << ' ' << L->curr() << ' ';
-		L->seek(1);
+		cout << L->next() << ", ";
 	}
-	cout << '\n';
+	cout << "\b\b }\n";
 }
 
-/*
-        void push(T value);
-        void link(node<T>* A, node<T>* B);
-        bool EmptyCheck(node<T>* node);
-        void pop();
-        T first();
-        T next();
-        void seek(node<T>* node);
-        void seek(int times);
-        bool contains(T value);
-        int pos(T value);
-        int pos(node<T>* node);
-        T curr();
-*/
+void help()
+{
+    for (int t = 1; t <= sizeof(cmds)/sizeof(string); t++)
+        cout << "         " << cmds[t] << "\n" ;
+}
 
 const int map(string cmd)
 {
-	string cmds[] = {"default", "push", "pop", "first", "next", "seek", "contains", "pos", "curr", "perimeter", "cls", "exit"};
-	for (int t = 1; t < 11; t++)
+	for (int t = 1; t <= sizeof(cmds)/sizeof(string); t++)
 	{
 		if (cmds[t] == cmd)
 			return t;
@@ -44,54 +45,96 @@ int main()
 {
 	CDLL<int> d;
 	string c;
-	int arg, intc;
+	int arg, intc, arg2;
 
 	while (1)
 	{
-		cout << "\n/msc24x/- ";
+		cout << "/msc24x-CDLL-d-int-" << d.perimeter << "/- ";
 		cin >> c;
+		for(int i = 0; i <sizeof(c)/sizeof(char); i++)c[0+i] =  std::tolower(c[0+i]);
 		intc = map(c);
 
 		switch (intc)
-			{
-			case 1:{
-				cin >> arg;
-				d.push(arg);
-				break;}
-		case 2:{
+        {
+        case 1:
+            cin >> arg;
+            d.push(arg);
+            break;
+
+		case 2:
 			d.pop();
-			break;}
-		case 3:{
+			break;
+
+		case 3:
 			cout << d.first();
-			break;}
-		case 4:{
+			break;
+
+		case 4:
 			cout << d.next();
-			break;}
-		case 5:{
+			break;
+
+		case 5:
 			cin >> arg;
 			d.seek(arg);
-			break;}
-		case 6:{
+			break;
+
+		case 6:
 			cin >> arg;
 			cout << d.contains(arg);
-			break;}
-		case 7:{
+			break;
+
+		case 7:
 			cin >> arg;
 			cout << d.pos(arg);
-			break;}
-		case 8:{
+			break;
+		case 8:
 			cout << d.curr();
-			break;}
-		case 9:{
+			break;
+
+		case 9:
 			cout << d.perimeter;
-			break;}
-		case 10:{
-			//system(cls);
-			break;}
-		case 11:{
-			return 0;}
-		default:{
-			cout << "unrecognized command\n";}
+			break;
+
+		case 10:
+			system("cls");
+			break;
+
+		case 11:
+			return 0;
+
+        case 12:
+            cout << d.prev();
+            break;
+
+        case 13:
+            cin >> arg >> arg2;
+            d.insert(arg, arg2);
+            break;
+
+        case 14:
+            cin >> arg;
+            d.destroy(arg);
+            break;
+
+        case 15:
+            cin >> arg;
+            d.destroyPos(arg);
+            break;
+
+        case 16:
+            printit(&d);
+            break;
+
+        case 17:
+            help();
+            break;
+
+        case 18:
+            d.wash();
+            break;
+
+		default:
+			cout << "unrecognized command: try using \"help\"\n";
 		}
 	}
 	return 0;
